089b7a72cab4639c5e298aa9e7642d3015a621b5
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-25 09:14:06 +0330

    Page navigation smooth scroll using event delegation

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,8 +33,17 @@ document.addEventListener('keydown', function (e) {
 });
 
 // Smooth scroll to section 1
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
 btnScrollTo.addEventListener("click", () => {
   section1.scrollIntoView({behavior: "smooth"});
 });
+
+// Page navigation smooth scroll using event delegation:
+document.querySelector(".nav__links").addEventListener(
+  "click", function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains("nav__link")) {
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({behavior:"smooth"});
+    }
+  }
+);
