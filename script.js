49a681967379ec85084066e15261ceff399f399f
EverLookNeverSee <EverLookNeverSee@Proton.Me>
2023-02-27 15:38:43 +0330

    Implementing reveal section using intersection observer api

--- script.js
+++ script.js
@@ -101,3 +101,22 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`
 });
 headerObserver.observe(header);
+
+
+// Reveal section:
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15
+});
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
